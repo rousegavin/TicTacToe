8d0e664544d13de95b703553d291b75bcf16d891
Gavin Rouse <60052977+rousegavin@users.noreply.github.com>
2020-11-29 22:28:19 -0800

    Add files via upload

---
+++ TicTacToe/TicTacToe/GameBoard.cs
@@ -0,0 +1,146 @@
+﻿/* Gavin Rouse
+ * This class contains the implementation of the tic tac toe gameboard.
+ * It consists of a canvas and a list of squares that are on the canvas.
+ */
+
+using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TicTacToe
+{
+    class GameBoard
+    {
+        Canvas gameBoard;
+        ArrayList squares;
+
+        public GameBoard(Canvas c)
+        {
+            gameBoard = c;
+            squares = new ArrayList();
+            InitializeSquares();
+        }
+
+        private void InitializeSquares()
+        {
+            Point p = new Point(0, 0);
+            squares.Add(new Square(p, 117, 97));
+            p.X = 122;
+            squares.Add(new Square(p, 115, 97));
+            p.X = 242;
+            squares.Add(new Square(p, 113, 97));
+            p.X = 0;
+            p.Y = 102;
+            squares.Add(new Square(p, 117, 95));
+            p.X = 122;
+            squares.Add(new Square(p, 115, 95));
+            p.X = 242;
+            squares.Add(new Square(p, 113, 95));
+            p.X = 0;
+            p.Y = 202;
+            squares.Add(new Square(p, 117, 93));
+            p.X = 122;
+            squares.Add(new Square(p, 115, 93));
+            p.X = 242;
+            squares.Add(new Square(p, 113, 93));
+        }
+
+        public Square GetSquare(int i)
+        {
+            return (Square)squares[i];
+        }
+
+        public Point HitSquare(MouseButtonEventArgs e, char turn)
+        {
+            foreach (Square s in squares)
+            {
+                double x = e.GetPosition(gameBoard).X;
+                double y = e.GetPosition(gameBoard).Y;
+
+                if (x <= s.TopLeft.X + s.XWidth && x >= s.TopLeft.X)
+                {
+                    if (y <= s.TopLeft.Y + s.YWidth && y >= s.TopLeft.Y)
+                    {
+                        if (!s.Filled)
+                        {
+                            s.Filled = true;
+                            s.Choice = turn;
+                            return s.TopLeft;
+                        }
+                    }
+                }
+            }
+
+            return new Point(-1, -1);
+        }
+
+        public bool IsFull()
+        {
+            foreach (Square s in squares)
+            {
+                if (s.Filled == false)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public char IsWin()
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                Square s1 = (Square)squares[i];
+                Square s2 = (Square)squares[i + 3];
+                Square s3 = (Square)squares[i + 6];
+
+                if (s1.Choice != 'Z')
+                {
+                    if (s1.Choice == s2.Choice && s1.Choice == s3.Choice)
+                        return s1.Choice;
+                }
+            }
+
+            for (int i = 0; i <= 6; i += 3)
+            {
+                Square s1 = (Square)squares[i];
+                Square s2 = (Square)squares[i + 1];
+                Square s3 = (Square)squares[i + 2];
+
+                if (s1.Choice != 'Z')
+                {
+                    if (s1.Choice == s2.Choice && s1.Choice == s3.Choice)
+                        return s1.Choice;
+                }
+            }
+
+            for (int i = 0; i == 0; i++)
+            {
+                Square s1 = (Square)squares[i];
+                Square s2 = (Square)squares[i + 4];
+                Square s3 = (Square)squares[i + 8];
+
+                if (s1.Choice != 'Z')
+                {
+                    if (s1.Choice == s2.Choice && s1.Choice == s3.Choice)
+                        return s1.Choice;
+                }
+            }
+
+            for (int i = 2; i == 2; i++)
+            {
+                Square s1 = (Square)squares[i];
+                Square s2 = (Square)squares[i + 2];
+                Square s3 = (Square)squares[i + 4];
+
+                if (s1.Choice != 'Z')
+                {
+                    if (s1.Choice == s2.Choice && s1.Choice == s3.Choice)
+                        return s1.Choice;
+                }
+            }
+
+            return 'N';
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,254 @@
+﻿/* Gavin Rouse
+ * This class contains all of the event handlers for the main window,
+ * as well as all necessary supporting methods.
+ */
+
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GameBoard gameBoard;
+        char turn = 'X';
+        bool coinFlip = false;
+        bool won = false;
+        bool full = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeEvents();
+            gameBoard = new GameBoard(ticTacToeCanvas);
+        }
+
+        private void InitializeEvents()
+        {
+            ticTacToeCanvas.MouseDown += new MouseButtonEventHandler(BoardMouseClick);
+            coinTossButton.Click += new RoutedEventHandler(CoinTossButtonClick);
+            resetButton.Click += new RoutedEventHandler(ResetButtonClick);
+            coinTossMenu.Click += new RoutedEventHandler(CoinTossButtonClick);
+            resetMenu.Click += new RoutedEventHandler(ResetButtonClick);
+            onePlayerMenu.Click += new RoutedEventHandler(OnePlayerMenuClick);
+            twoPlayerMenu.Click += new RoutedEventHandler(TwoPlayerMenuClick);
+            aboutMenu.Click += new RoutedEventHandler(AboutMenuClick);
+            rulesMenu.Click += new RoutedEventHandler(RulesMenuClick);
+        }
+
+        private void BoardMouseClick(object sender, MouseButtonEventArgs e)
+        {
+            if (!won && !full && coinFlip)
+            {
+                Point p = gameBoard.HitSquare(e, turn);
+
+                if (p.X == -1 && p.Y == -1)
+                    return;
+
+                DrawShape(p);
+                UpdateStatus();
+                IsFull();
+                IsWin();
+
+                if (onePlayerButton.IsChecked == true && !won && !full)
+                    AIMove();
+            }
+        }
+
+        private void CoinTossButtonClick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            if (rand.Next(1, 3) == 1)
+                turn = 'X';
+
+            else
+                turn = 'O';
+
+            coinTossButton.IsEnabled = false;
+            coinTossMenu.IsEnabled = false;
+            onePlayerButton.IsEnabled = false;
+            twoPlayerButton.IsEnabled = false;
+            onePlayerMenu.IsEnabled = false;
+            twoPlayerMenu.IsEnabled = false;
+            coinFlip = true;
+            UpdateStatus();
+
+            if (onePlayerButton.IsChecked == true)
+            {
+                if (turn == 'O')
+                    AIMove();
+            }
+        }
+
+        private void ResetButtonClick(object sender, EventArgs e)
+        {
+            ticTacToeCanvas.Children.RemoveRange(4, ticTacToeCanvas.Children.Count);
+            gameBoard = new GameBoard(ticTacToeCanvas);
+            coinTossButton.IsEnabled = true;
+            onePlayerButton.IsEnabled = true;
+            twoPlayerButton.IsEnabled = true;
+            coinTossMenu.IsEnabled = true;
+            onePlayerMenu.IsEnabled = true;
+            twoPlayerMenu.IsEnabled = true;
+            statusLabel.Content = "Flip The Coin";
+            won = false;
+            full = false;
+            coinFlip = false;
+        }
+
+        private void OnePlayerMenuClick(object sender, EventArgs e)
+        {
+            twoPlayerButton.IsChecked = false;
+            twoPlayerMenu.IsChecked = false;
+            onePlayerButton.IsChecked = true;
+            onePlayerMenu.IsChecked = true;
+        }
+
+        private void TwoPlayerMenuClick(object sender, EventArgs e)
+        {
+            onePlayerButton.IsChecked = false;
+            onePlayerMenu.IsChecked = false;
+            twoPlayerButton.IsChecked = true;
+            twoPlayerMenu.IsChecked = true;
+        }
+
+        private void AboutMenuClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developer: Gavin Rouse\n\nVersion: 1.0\n\n.NET Framework Version: 4.5.2\n\nBit Version: 32-bit");
+        }
+
+        private void RulesMenuClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("The object of Tic Tac Toe is to get three in a row. You play on a three by three game board."
+                + " The first player is known as X and the second is O. Players alternate placing Xs and Os on the game"
+                + " board until either opponent has three in a row or all nine squares are filled.\n\nFirst, choose 'One "
+                + "Player' if you would like to play against the computer or 'Two Players' if you "
+                + "and another person would like to play. Then, click 'Coin Toss' to determine who will go first. Then, "
+                + "take turns clicking a square to play in. If someone wins or the board fills up, click 'Reset' to "
+                + "reset the game.");
+        }
+
+        private void DrawShape(Point p)
+        {
+            if (turn == 'X')
+            {
+                Line l1 = new Line();
+                l1.X1 = p.X + 15;
+                l1.Y1 = p.Y + 15;
+                l1.X2 = p.X + 102;
+                l1.Y2 = p.Y + 82;
+                l1.Stroke = Brushes.Blue;
+                l1.StrokeThickness = 5;
+                Line l2 = new Line();
+                l2.X1 = p.X + 102;
+                l2.Y1 = p.Y + 15;
+                l2.X2 = p.X + 15;
+                l2.Y2 = p.Y + 82;
+                l2.Stroke = Brushes.Blue;
+                l2.StrokeThickness = 5;
+
+                ticTacToeCanvas.Children.Add(l1);
+                ticTacToeCanvas.Children.Add(l2);
+                turn = 'O';
+                return;
+            }
+
+            else if (turn == 'O')
+            {
+                Ellipse e = new Ellipse();
+                e.Stroke = Brushes.Red;
+                e.StrokeThickness = 5;
+                e.Width = 63;
+                e.Height = e.Width;
+                e.Margin = new Thickness(p.X + 57 / 2, p.Y + 37 / 2, 0, 0);
+
+                ticTacToeCanvas.Children.Add(e);
+                turn = 'X';
+                return;
+            }
+        }
+
+        private void UpdateStatus()
+        {
+            if (twoPlayerButton.IsChecked == true)
+            {
+                if (turn == 'X')
+                    statusLabel.Content = "Player One's Turn";
+
+                else
+                    statusLabel.Content = "Player Two's Turn";
+            }
+
+            else if (onePlayerButton.IsChecked == true)
+            {
+                if (turn == 'X')
+                    statusLabel.Content = "Your Turn";
+
+                else
+                    statusLabel.Content = "Computer's Turn";
+            }
+        }
+
+        private void AIMove()
+        {
+            bool chosen = false;
+            Random rand = new Random();
+            int choice = 0;
+
+            while (!chosen)
+            {
+                choice = rand.Next(0, 9);
+                Square s = gameBoard.GetSquare(choice);
+
+                if (!s.Filled)
+                {
+                    s.Filled = true;
+                    s.Choice = turn;
+                    DrawShape(s.TopLeft);
+                    chosen = true;
+                }
+            }
+            
+            UpdateStatus();
+            IsFull();
+            IsWin();
+        }
+
+        private void IsFull()
+        {
+            full = gameBoard.IsFull();
+        }
+        
+        private void IsWin()
+        {
+            char c = gameBoard.IsWin();
+
+            if (c == 'N')
+            {
+                if (full)
+                    statusLabel.Content = "It's A Draw!";
+
+                return;
+            }
+
+            if (c == 'X')
+            {
+                won = true;
+                statusLabel.Content = "Player One Wins!";
+            }
+
+            if (c == 'O')
+            {
+                won = true;
+                statusLabel.Content = "Player Two Wins!";
+            }
+        }
+    }
+}
